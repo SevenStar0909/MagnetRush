de4869acb84db5122875ad2dffd3a840adee26e7
Touga9910 <touga9910@gmail.com>
2026-03-23 13:43:19 +0900

    Auto-commit before update: 2026/03/23 13:43:19

--- Magnet_Rush/Assets/_Project/Scripts/Data/BulletSettings.cs
+++ Magnet_Rush/Assets/_Project/Scripts/Data/BulletSettings.cs
@@ -7,4 +7,12 @@ public class BulletSettings : ScriptableObject
     public float bulletSpeed = 30f;
     public float lifetime = 5f;
     public int maxBullets = 4;
+
+    [Header("Materials(Bullet)")]
+    public Material sMaterial;  // S極用のマテリアル
+    public Material nMaterial;  // N極用のマテリアル
+
+    [Header("Materials(Magnetic Field)")]
+    public Material sFieldMaterial;
+    public Material nFieldMaterial;
 }
