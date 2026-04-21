57d16868363820707b6287ac63d589cb57792a4e
西川駿太 <takecc841@gmail.com>
2026-04-21 22:01:20 +0900

    refactor(state): Player の能力呼び出しを Controller 直接呼び出しに変更

--- Magnet_Rush/Assets/_Project/Scripts/Core/Player/States/IdlePlayerState.cs
+++ Magnet_Rush/Assets/_Project/Scripts/Core/Player/States/IdlePlayerState.cs
@@ -1,4 +1,4 @@
-﻿/// <summary>
+/// <summary>
 /// プレイヤーの待機ステート。移動入力で移動ステートに遷移する。
 /// </summary>
 public class IdlePlayerState : EntityState<Player>
@@ -6,11 +6,11 @@ public class IdlePlayerState : EntityState<Player>
     public override void UpdateState(float dt)
     {
         m_entity.SlowDown(dt);
-        m_entity.SwitchPole();
-        m_entity.HandleAimInput();
-        m_entity.Fire();
-        m_entity.SelfFire();
-        m_entity.Reload();
+        m_entity.polarity.Switch();
+        m_entity.aim.HandleAimInput();
+        m_entity.shooting.Fire();
+        m_entity.shooting.SelfFire();
+        m_entity.shooting.Reload();
 
         if (m_entity.input.MoveInput.sqrMagnitude > 0.01f)
         {
--- Magnet_Rush/Assets/_Project/Scripts/Core/Player/States/MovePlayerState.cs
+++ Magnet_Rush/Assets/_Project/Scripts/Core/Player/States/MovePlayerState.cs
@@ -1,4 +1,4 @@
-﻿/// <summary>
+/// <summary>
 /// プレイヤーの移動ステート。入力がなくなると待機ステートに遷移する。
 /// </summary>
 public class MovePlayerState : EntityState<Player>
@@ -6,11 +6,11 @@ public class MovePlayerState : EntityState<Player>
     public override void UpdateState(float dt)
     {
         m_entity.AccelerateToInputDirection(dt);
-        m_entity.SwitchPole();
-        m_entity.HandleAimInput();
-        m_entity.Fire();
-        m_entity.SelfFire();
-        m_entity.Reload();
+        m_entity.polarity.Switch();
+        m_entity.aim.HandleAimInput();
+        m_entity.shooting.Fire();
+        m_entity.shooting.SelfFire();
+        m_entity.shooting.Reload();
 
         if (m_entity.input.MoveInput.sqrMagnitude < 0.01f)
         {
